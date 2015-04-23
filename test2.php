bf64beb0b8d1ecb1686dd0c2859af73ff4976162
JustinXyrel <jx.dorongon@dot.ph>
2015-04-23 14:25:17 +0800

    add test2

---
+++ test2.php
@@ -0,0 +1,15 @@
+<?php
+	$func = new Func;
+		$S = 'BBBB';
+		$str_count = strlen($S);
+		while($str_count > 2){
+          
+				$S = $func->get_value($S);	
+				$str_count = strlen($S);
+			
+		}
+		$data['fin_val'] = $S;
+		echo $S;
+
+	
+	?>
